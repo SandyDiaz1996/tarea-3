c05f7da7c17585341b8961c6cfd409940c007019
SandyDiaz1996 <sdiazo@unemi.edu.ec>
2021-06-11 16:02:18 -0500

    Ejercicio 1 presentacion de datos

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -4,3 +4,8 @@ class Condicion:
     def __init__(self, num1=0, num2=0):
         self.numero1 = num1
         self.numero2 = num2
+
+
+condicion1 = Condicion()
+print(condicion1.numero1)
+print(condicion1.numero2)
