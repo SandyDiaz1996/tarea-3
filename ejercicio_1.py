7d1bc58f45c66449f8f33bbde189b67c8edc0e2e
SandyDiaz1996 <sdiazo@unemi.edu.ec>
2021-06-11 16:09:58 -0500

    Ejercicio 2 creo clase condicion

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -4,8 +4,18 @@ class Condicion:
     def __init__(self, num1=0, num2=0):
         self.numero1 = num1
         self.numero2 = num2
+        # numero = num1 + num2
+        self.numero3 = num1
+    
+    def usoIf(self):
+        if self.numero1 == self.numero2:
+            print("numero1: {}, numero2: {}; Son Iguales.".format(self.numero1, self.numero2))
+        elif self.numero1 == self.numero3:
+            print("numero1: {}, numero3: {}; Son Iguales.".format(self.numero1, self.numero3))
+        else:
+            print("No son iguales.")    
 
 
-condicion1 = Condicion()
-print(condicion1.numero1)
-print(condicion1.numero2)
+# condicion1 = Condicion()
+# print(condicion1.numero1)
+# print(condicion1.numero2)
